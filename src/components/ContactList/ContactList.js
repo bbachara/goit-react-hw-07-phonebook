5d4774ac73118a09e8e0edc37968ced7c8bb5caa
Bartek Bachara <bbachara@poczta.onet.pl>
2024-05-25 10:50:58 +0200

    react-hw-07-phonebook upd 2

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,13 +1,78 @@
+// import { useEffect } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { Contact } from '../Contact/Contact';
+// import {
+//   getContacts,
+//   getStatusFilter,
+//   getSearchQuery,
+// } from '../../redux/selectors';
+// import css from './ContactList.module.css';
+// import { statusFilters } from '../../redux/constants';
+
+// const getVisibleContacts = (contacts, statusFilter, searchQuery) => {
+//   let filteredContacts = contacts;
+
+//   if (searchQuery) {
+//     filteredContacts = filteredContacts.filter(contact =>
+//       contact.name.toLowerCase().includes(searchQuery.toLowerCase())
+//     );
+//   }
+
+//   switch (statusFilter) {
+//     case statusFilters.active:
+//       return filteredContacts.filter(contact => !contact.completed);
+//     case statusFilters.completed:
+//       return filteredContacts.filter(contact => contact.completed);
+//     default:
+//       return filteredContacts;
+//   }
+// };
+
+// export const ContactList = () => {
+//   const dispatch = useDispatch();
+//   const contacts = useSelector(getContacts);
+//   const statusFilter = useSelector(getStatusFilter);
+//   const searchQuery = useSelector(getSearchQuery);
+//   const visibleContacts = getVisibleContacts(
+//     contacts,
+//     statusFilter,
+//     searchQuery
+//   );
+
+//   useEffect(() => {
+//     const storedContacts = localStorage.getItem('contacts');
+//     if (storedContacts) {
+//       const parsedContacts = JSON.parse(storedContacts);
+
+//       dispatch({ type: 'contacts/loadContacts', payload: parsedContacts });
+//     }
+//   }, [dispatch]);
+
+//   return (
+//     <div>
+//       <h2 className={css.title}>List of Contacts</h2>
+//       <ul className={css.list}>
+//         {visibleContacts.map(contact => (
+//           <li className={css.listItem} key={contact.id}>
+//             <Contact contact={contact} />
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Contact } from '../Contact/Contact';
+import { fetchContacts } from '../../redux/operations';
 import {
   getContacts,
   getStatusFilter,
   getSearchQuery,
 } from '../../redux/selectors';
-import css from './ContactList.module.css';
 import { statusFilters } from '../../redux/constants';
+import css from './ContactList.module.css';
 
 const getVisibleContacts = (contacts, statusFilter, searchQuery) => {
   let filteredContacts = contacts;
@@ -30,7 +95,7 @@ const getVisibleContacts = (contacts, statusFilter, searchQuery) => {
 
 export const ContactList = () => {
   const dispatch = useDispatch();
-  const contacts = useSelector(getContacts);
+  const { items: contacts, isLoading, error } = useSelector(getContacts);
   const statusFilter = useSelector(getStatusFilter);
   const searchQuery = useSelector(getSearchQuery);
   const visibleContacts = getVisibleContacts(
@@ -40,17 +105,14 @@ export const ContactList = () => {
   );
 
   useEffect(() => {
-    const storedContacts = localStorage.getItem('contacts');
-    if (storedContacts) {
-      const parsedContacts = JSON.parse(storedContacts);
-
-      dispatch({ type: 'contacts/loadContacts', payload: parsedContacts });
-    }
+    dispatch(fetchContacts());
   }, [dispatch]);
 
   return (
     <div>
       <h2 className={css.title}>List of Contacts</h2>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
       <ul className={css.list}>
         {visibleContacts.map(contact => (
           <li className={css.listItem} key={contact.id}>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,13 @@
-import { createStore } from "redux";
-import { devToolsEnhancer } from "@redux-devtools/extension";
-import { rootReducer } from "./reducer";
+// import { createStore } from "redux";
+// import { devToolsEnhancer } from "@redux-devtools/extension";
+// import { rootReducer } from "./reducer";
 
-const enhancer = devToolsEnhancer();
-export const store = createStore(rootReducer, enhancer);
+// const enhancer = devToolsEnhancer();
+// export const store = createStore(rootReducer, enhancer);
+
+import { configureStore } from '@reduxjs/toolkit';
+import { rootReducer } from './rootReducer';
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
